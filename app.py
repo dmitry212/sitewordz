ea5de25add49528918977c568c20cd2464e05930
dmitry212 <dmitry212@gmail.com>
2023-12-15 15:52:51 -0500

    v0 of ui + starting db lookup logic

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 """ sitewordz app! """
-from flask import Flask
+import sqlite3
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Welcome to Sitewordz!'
+@app.route('/get_logged_in_user')
+def get_logged_in_user():
+    conn = sqlite3.connect('sitewordz.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE is_logged_in = 1")
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "No logged-in user found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)

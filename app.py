c615661972d829939841d64e97488d1a180988e1
dmitry212 <dmitry212@gmail.com>
2023-12-19 14:22:33 -0500

    pull user from db, db has lists and user table, app.py runs game screen

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 """ sitewordz app! """
 import sqlite3
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, session, render_template
 
 app = Flask(__name__)
+app.secret_key ='2hKgJ4kbZMoWAQ-'
 
-@app.route('/get_logged_in_user')
-def get_logged_in_user():
-    conn = sqlite3.connect('sitewordz.db')
+def get_current_user():
+    """loads currently logged in user"""
+    conn = sqlite3.connect('db/sitewordz.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE is_logged_in = 1")
+    cursor.execute("SELECT user_id, name FROM users WHERE is_logged_in = 1")
     user = cursor.fetchone()
     conn.close()
-
+    print(f"user: {user}, type(user): {type(user)}")
+    session['user_id'] = user[0]
     if user:
-        return jsonify(user)
+        return user[1]
     else:
         return jsonify({"error": "No logged-in user found"}), 404
+
+@app.route('/')
+def play_game():
+    """main gameç logic"""
+    user = get_current_user()
+    return render_template('web/game_play.html', user_name=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
